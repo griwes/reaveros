a1728a0e8d9129ec4594b5c15842e2d05eb89bfe
Reaver Project CI <ci@reaver-project.org>
2021-07-15 04:04:21 +0000

    Update toolchain cmake from v3.20.5 to v3.21.0.

--- toolchains/config.cmake
+++ toolchains/config.cmake
@@ -1,5 +1,5 @@
 set(REAVEROS_CMAKE_REPO https://github.com/Kitware/CMake)
-set(REAVEROS_CMAKE_TAG v3.20.5)
+set(REAVEROS_CMAKE_TAG v3.21.0)
 set(REAVEROS_CMAKE_PATTERN v[0-9]+\.[0-9]+\.[0-9]+)
 set(REAVEROS_CMAKE_EXCLUDE "")
 
